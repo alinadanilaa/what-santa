b95dab8de691f2028ce6ac977b48a9f35adc2b7d
Alina <alinadanila1643@gmail.com>
2023-12-11 13:03:21 +0200

    update: img

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -13,34 +13,36 @@ const Gallery = ({ cities }) => {
     };
 
     return (
-        <div className="gallery-container flex flex-wrap justify-evenly">
-            {cities.map((city) => (
-                <div
-                    key={city.id}
-                    className="gallery-item w-32 h-32 bg-cover m-2 cursor-pointer"
-                    onClick={() => handleCityClick(city)}
-                    style={{ backgroundImage: `url(${city.image})` }}
-                />
-            ))}
-
-            {selectedCity && (
-                <div
-                    className="zoomed-image-container fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-80"
-                    onClick={handleCloseButtonClick}
-                >
+        <>
+            <div className='flex justify-center items-center'>What Santa Are You Today?</div>
+            <div className="gallery-container flex flex-wrap justify-center">
+                {cities.map((city) => (
                     <div
-                        className="zoomed-image max-w-full max-h-full bg-contain bg-no-repeat cursor-pointer"
-                        style={{ backgroundImage: `url(${selectedCity.image})` }}
+                        key={city.id}
+                        className="gallery-item w-32 h-32 bg-cover cursor-pointer"
+                        onClick={() => handleCityClick(city)}
+                        style={{ backgroundImage: `url(${city.image})` }}
                     />
-                    <img
+                ))}
 
-                        src={`${selectedCity.image}`}
-                        alt="icon"
-                    />
-                </div>
-            )}
-        </div>
-    );
+                {selectedCity && (
+                    <div
+                        className="zoomed-image-container fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-80"
+                        onClick={handleCloseButtonClick}
+                    >
+                        <div
+                            className="zoomed-image max-w-full max-h-full bg-contain bg-no-repeat cursor-pointer"
+                            style={{ backgroundImage: `url(${selectedCity.image})` }}
+                        />
+                        <img
+
+                            src={`${selectedCity.image}`}
+                            alt="icon"
+                        />
+                    </div>
+                )}
+            </div>
+        </>);
 };
 
 export default Gallery;
