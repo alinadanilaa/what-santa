021ad9a783e0a8b5b1d92a3dbc1466c2577ccb14
Alina <alinadanila1643@gmail.com>
2023-12-11 11:02:56 +0200

    add: pics

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,27 @@ const cities = [
   {
     id: 1,
     title: 'Santa 1',
-    image: './public/santa1.jpg',
+    image: './public/santa1.png',
   },
   {
     id: 2,
     title: 'Santa 2',
-    image: './public/santa2.jpg',
+    image: './public/santa2.png',
   },
   {
     id: 3,
     title: 'Santa 3',
-    image: './public/santa1.jpg',
+    image: './public/santa1.png',
   },
   {
     id: 4,
     title: 'Santa 4',
-    image: './public/santa2.jpg',
+    image: './public/santa2.png',
   },
   {
     id: 5,
     title: 'Santa 5',
-    image: './public/santa1.jpg',
+    image: './public/santa1.png',
   },
 ];
 
