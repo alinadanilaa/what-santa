48b42bb88419389d41c588789cb37f4de04cfe34
Alina <alinadanila1643@gmail.com>
2023-12-11 18:44:56 +0200

    update: img

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,15 @@ import React from 'react';
 import './App.css';
 import Gallery from './Gallery';
 
-const santas = [
-  {
-    id: 1,
-    title: 'Santa 1',
-    image: './public/1.png',
-  },
-  {
-    id: 2,
-    title: 'Santa 2',
-    image: './public/2.png',
-  },
-  {
-    id: 3,
-    title: 'Santa 3',
-    image: './public/3.png',
-  },
-  {
-    id: 4,
-    title: 'Santa 4',
-    image: './public/4.png',
-  },
-  {
-    id: 5,
-    title: 'Santa 5',
-    image: './public/5.png',
-  },
-  {
-    id: 6,
-    title: 'Santa 6',
-    image: './public/6.png',
-  },
-];
+const santas = [];
+
+for (let i = 1; i <= 40; i++) {
+  santas.push({
+    id: i,
+    title: `Santa ${i}`,
+    image: `./${i}.png`,
+  });
+}
 
 const App = () => {
   return (
